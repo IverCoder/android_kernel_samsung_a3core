32c1ea784e5ee57388dba411b9de404d7d522994
IverCoder <ivercoder@proton.me>
2024-10-31 17:16:28 +0800

    mm: remove excess argument when invoking cow_user_page

--- mm/memory.c
+++ mm/memory.c
@@ -2767,7 +2767,7 @@ static int wp_page_copy(struct vm_fault *vmf)
 				vmf->address);
 		if (!new_page)
 			goto out;
-		cow_user_page(new_page, old_page, vmf->address, vma);
+		cow_user_page(new_page, old_page, vma);
 	}
 
 	if (mem_cgroup_try_charge(new_page, mm, GFP_KERNEL, &memcg, false))
